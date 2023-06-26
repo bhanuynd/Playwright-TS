2521b3a558bad6714cdb1903f0f3b5fba2945cae
bhanu <bhanuprakashynd61@gmail.com>
2023-06-26 20:22:24 +0530

    added cucumber.js cucumber

---
+++ cucumber.js
@@ -0,0 +1,9 @@
+const config ={
+    paths: ['src/**/*.feature'],
+    require: ['src/**/*.js'],
+    formatOptions:{
+        snippetInterface: 'async-await'
+    },
+    publishQuiet:true,
+    dryRun:true
+}
